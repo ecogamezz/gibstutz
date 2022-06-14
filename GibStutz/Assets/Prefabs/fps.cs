57532c7a06e54f8ff7a1990a97c5e0deace7b936
ecogamezz <erikoberle04@gmail.com>
2022-06-14 22:53:33 +0200

    fixed gun (now working)

---
+++ GibStutz/Assets/Prefabs/fps.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SetTargetFrameRate : MonoBehaviour
+{
+    public int targetFrameRate = 120;
+
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+    }
+}
