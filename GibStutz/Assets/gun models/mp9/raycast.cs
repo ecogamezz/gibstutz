538e46209502c26bcf82ca82526127cce7c302a1
Cigdan <72510831+Cigdan@users.noreply.github.com>
2022-06-16 16:47:28 +0200

    fixed the ar model & added a basic raycast

---
+++ GibStutz/Assets/gun models/mp9/raycast.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class raycast : MonoBehaviour
+{
+    [SerializeField] XRGrabInteractable grabInteractable;
+    [SerializeField] Transform raycastOrigin;
+    [SerializeField] LayerMask targetLayer;
+
+    private void FireRaycastIntoScene()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(raycastOrigin.position, raycastOrigin.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, targetLayer))
+        {
+            Debug.Log("hit target");
+        }
+   
+    }
+    public void shoot()
+    {
+
+        FireRaycastIntoScene();
+    }
+
+}
