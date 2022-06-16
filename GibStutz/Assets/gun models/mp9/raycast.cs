0e5031d401399811930a5f8374d14b8e00422bfd
ecogamezz <erikoberle04@gmail.com>
2022-06-16 19:20:24 +0200

    test to oush blocks if you shoot at them

--- GibStutz/Assets/gun models/mp9/raycast.cs
+++ GibStutz/Assets/gun models/mp9/raycast.cs
@@ -12,15 +12,22 @@ public class raycast : MonoBehaviour
     [SerializeField] AudioClip gunClipSFX;
     int FireRate = 10;
 
-    
+    public float thrust = 1.0f;
+    public Rigidbody rb;
 
-    private void FireRaycastIntoScene()
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void FireRaycastIntoScene()
     {
         RaycastHit hit;
 
         if(Physics.Raycast(raycastOrigin.position, raycastOrigin.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, targetLayer))
         {
             Debug.Log("hit target");
+            rb.AddForce(100, 100, thrust, ForceMode.Impulse);
         }
     
     }
