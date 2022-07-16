25fe237b4a2b7c4435c02e3d3bd8e435bc704c51
ecogamezz <erikoberle04@gmail.com>
2022-07-16 18:41:51 +0200

    added new everything with new version

---
+++ Dont Hesitate/Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+    
+    public GameObject destroyedVersion;
+    public GameObject destroyedVersion2;
+    public ParticleSystem particlesys;
+
+    public void launch()
+    {
+        Debug.Log("meabootlegotshot");
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Instantiate(destroyedVersion2, transform.position, transform.rotation);
+        Instantiate(particlesys, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+    }
+
+    
+
+}
+
+
